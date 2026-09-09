62ff89f1a6c30538b5c9f1598b77c4f5bb6ffbc9
gamerlfgg1 <gamerlf80@gmail.com>
2026-09-09 11:47:49 -0300

    a

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => (
     const likeBtn = document.querySelector(".like-btn");
     const postMedia = document.querySelector(".post-media");
-    if (!likeBtn) = return;
+    if (!likeBtn) return;
 
     const likesCountSpan = likeBtn.querySelector(".likes-count");
     const bookmarkBtn = document.querySelector(".bookmark-btn");
 
     let isLiked = false;
     let baseLikes = 0;
+
+    //
+    if(likesCountSpan){
+            likesCountSpan.textContent = "0";
+    }
+
+//numeros >=1000
+function formatLikes(num) {
+    if (num >=1000){
+        return (num/1000).toFixed(1) + "K";
+    }
+    return num.toString();
+
+
+
+}
+
 ))
